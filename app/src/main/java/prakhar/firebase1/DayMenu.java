cc3f406dcbe5a4d0b761d8110a696239f4d4ac78
prakharag <prakharag28597@gmail.com>
2017-07-14 11:34:22 +0530

    menu app updated

---
+++ app/src/main/java/prakhar/firebase1/DayMenu.java
@@ -0,0 +1,24 @@
+package prakhar.firebase1;
+
+import java.util.ArrayList;
+
+/**
+ * Created by prakharag on 14-07-2017.
+ */
+
+
+public class DayMenu {
+    private String day;
+    private ArrayList<Bus> menulist;
+
+    public DayMenu(){
+    }
+
+    public String getDay() {
+        return day;
+    }
+    public ArrayList<Bus> getMenulist() {
+        return menulist;
+    }
+}
+
--- app/src/main/java/prakhar/firebase1/MessMenu.java
+++ app/src/main/java/prakhar/firebase1/MessMenu.java
@@ -3,17 +3,24 @@ package prakhar.firebase1;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by prakharag on 13-07-2017.
  */
 
 public class MessMenu extends AppCompatActivity {
+    DatabaseReference myRef;
     private static final String FIREBASE_STRING ="https://testfirebase-698a9.firebaseio.com/";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,8 +32,29 @@ public class MessMenu extends AppCompatActivity {
                 .setDatabaseUrl("https://testfirebase-698a9.firebaseio.com/")
                 .build();
         FirebaseApp menuApp = FirebaseApp.initializeApp(getApplicationContext(), options, "menuApp");
-        FirebaseDatabase secondDatabase = FirebaseDatabase.getInstance(menuApp);
-        secondDatabase.getReference().setValue(ServerValue.TIMESTAMP);
+
+        FirebaseDatabase menuDatabase = FirebaseDatabase.getInstance(menuApp);
+
+        //secondDatabase.getReference().setValue(ServerValue.TIMESTAMP);
+        myRef=menuDatabase.getReference();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    DayMenu day = postSnapshot.getValue(DayMenu.class);
+                    String today=day.getDay();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
         
 
 
